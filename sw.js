525b2467757f03525333a2c10679654c774ccc1f
TAEHO92 <taeho0717@naver.com>
2026-08-31 10:00:41 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+// 화면 파일만 캐시합니다. 데이터는 항상 서버에서 새로 받아옵니다.
+const CACHE = 'bbalsi-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== 'GET') return;
+  if (url.hostname.indexOf('google.com') !== -1) return; // 데이터 요청은 캐시하지 않음
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return r;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
